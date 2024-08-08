0c8c9482f9df938934142f730dc21282e04e384a
Azqiadistya <azqiadistya.aa@gmail.com>
2024-08-08 09:58:33 +0700

    add update

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,22 @@
 'use client';
-const HomePage = ({ users }) => (
+const HomePage = ({ posts }) => (
   <div>
     <h1>Todos</h1>
     <ul>
-      {users.map((users) => (
-        <li key={users.id}>{users.title}</li>
+      {posts.map((post) => (
+        <li key={post.id}>{post.title}</li>
       ))}
     </ul>
   </div>
 );
 
-HomePage.getInitialProps = async () => {
+export async function getStaticProps() {
+  // Fetch data during build time
   const res = await fetch("https://jsonplaceholder.typicode.com/todos");
-  const users = await res.json();
-  return { users };
-};
+  const posts = await res.json();
+
+  // Pass the fetched data to the page via props
+  return { props: { posts } };
+}
 
 export default HomePage;

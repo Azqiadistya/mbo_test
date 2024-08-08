519f3429e8cacdac9efd2d4e1f963c940d76aa99
Azqiadistya <azqiadistya.aa@gmail.com>
2024-08-08 10:04:53 +0700

    add update

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,41 @@
-'use client';
-const HomePage = ({ posts }) => (
-  <div>
-    <h1>Todos</h1>
-    <ul>
-      {posts.map((post) => (
-        <li key={post.id}>{post.title}</li>
-      ))}
-    </ul>
-  </div>
-);
+// 'use client';
+// const HomePage = (Page) => (
+//   <div>
+//     <h1>Todos</h1>
+//     <ul>
+//       {posts.map((post) => (
+//         <li key={post.id}>{post.title}</li>
+//       ))}
+//     </ul>
+//   </div>
+// );
 
-export async function getStaticProps() {
-  // Fetch data during build time
+// export async function Page() {
+//   // Fetch data during build time
+//   const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+//   const posts = await res.json();
+
+//   // Pass the fetched data to the page via props
+//   return { props: { posts } };
+// }
+
+// export default HomePage;
+async function getData() {
   const res = await fetch("https://jsonplaceholder.typicode.com/todos");
-  const posts = await res.json();
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
 
-  // Pass the fetched data to the page via props
-  return { props: { posts } };
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
 }
 
-export default HomePage;
+
+export default async function Page() {
+  const data = await getData();
+
+  return <main></main>;
+}
